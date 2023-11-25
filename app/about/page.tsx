1c598a8d33bb4c991bcea5a3384be95c3da097b7
William Phelps <williamphelps13@gmail.com>
2023-11-25 10:23:13 -0700

    Add demo pages to allow routing demo

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <section>
+      <h1>About</h1>
+      <h2>This is an about page</h2>
+      <Link href="/">Back home</Link>
+    </section>
+  );
+}
---
+++ app/favorites/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Favorites() {
+  return (
+    <section>
+      <h1>Favorites</h1>
+      <h2>This is a favorites page</h2>
+      <Link href="/">Back home</Link>
+    </section>
+  );
+}

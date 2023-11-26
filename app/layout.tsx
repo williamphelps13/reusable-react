677c0b377954744fe3a7d30b95141edb7e160e37
William Phelps <williamphelps13@gmail.com>
2023-11-25 18:49:37 -0700

    Test adding useEffect to handle page routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -6,7 +8,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pocket Somm",
-  description: "A handy app that guides users through somm style questions to arrive at a wine recommendation",
+  description:
+    "A handy app that guides users through somm style questions to arrive at a wine recommendation",
   keywords: ["Wine", "Recommendations", "Sommelier", "Wine Pairing"],
 };
 
@@ -15,6 +18,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    router.push(path).catch((err) => {
+      console.error("Routing error:", err);
+    });
+  }, [router]);
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>

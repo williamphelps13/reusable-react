872676c1b96a11f2667a61a1bcdb8ba580e3ef22
William Phelps <williamphelps13@gmail.com>
2023-12-04 11:09:50 -0700

    Add hover/focus/active to Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,9 +2,9 @@ import { twMerge } from 'tailwind-merge'
 
 type ButtonProps = {
   children: React.ReactNode
-  purpose: 'primary' | 'secondary'
-  size: 'sm' | 'md' | 'lg'
-  type: 'button' | 'reset' | 'submit'
+  purpose?: 'primary' | 'secondary'
+  size?: 'sm' | 'md' | 'lg'
+  type?: 'button' | 'reset' | 'submit'
 }
 
 export default function Button({
@@ -16,9 +16,11 @@ export default function Button({
   return (
     <button
       className={twMerge(
-        'rounded-lg py-2',
-        purpose === 'primary' && 'bg-blue-500 text-white',
-        purpose === 'secondary' && 'border-2 border-black text-black',
+        'rounded-lg py-2 transition-all',
+        purpose === 'primary' &&
+          'bg-blue-500  shadow-md hover:bg-blue-500/90 active:bg-blue-600 hover:shadow-lg text-white  focus-visible:outline-none focus:ring-2 focus:ring-blue-800',
+        purpose === 'secondary' &&
+          'border-2 border-black text-black hover:bg-gray-50',
         size === 'sm' && 'px-2  text-xs',
         size === 'md' && 'px-4 text-sm',
         size === 'lg' && 'px-6',

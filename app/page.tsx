44c4f12805b1749dbede0645a02e1f7b2f296c08
William Phelps <williamphelps13@gmail.com>
2023-12-04 11:09:25 -0700

    Start splash page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import Link from "next/link";
+import Link from 'next/link'
+import Button from '@/components/Button/Button'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
-      <nav className="space-x-8">
-        <Link href="/about">About</Link>
-        <Link href="/recipes">Recipes</Link>
-        <Link href="/favorites">Favorites</Link>
-        <br />
-      </nav>
-      <h1 className="text-xl">Home Page</h1>
+    <main className="w-screen h-screen bg-black flex flex-col justify-center items-center text-white">
+      <div className="p-40">
+        <h1 className="text-6xl mb-4">Pocket Somm</h1>
+        <p className="text-2xl text-gray-50 mb-8">
+          Wine recommendations right in your pocket.
+        </p>
+        <Link href="/somm">
+          <Button>Get started</Button>
+        </Link>
+      </div>
     </main>
-  );
+  )
 }

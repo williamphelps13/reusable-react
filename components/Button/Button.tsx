394949bb626fc743f4f1b7e8dfdf65aed50d483a
William Phelps <williamphelps13@gmail.com>
2023-12-09 10:02:12 -0700

    Run tw prettier on Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -20,7 +20,7 @@ export default function Button({
         purpose === 'primary' &&
           'bg-blue-500  shadow-md hover:bg-blue-500/90 active:bg-blue-600 hover:shadow-lg text-white  focus-visible:outline-none focus:ring-2 focus:ring-blue-800',
         purpose === 'secondary' &&
-          'border-2 border-black text-black hover:bg-gray-50',
+          'border-2 text-black border-black  hover:bg-gray-50',
         size === 'sm' && 'px-2  text-xs',
         size === 'md' && 'px-4 text-sm',
         size === 'lg' && 'px-6',

9f96ee064acaabd81ccfaeee47b22a211e84f38d
William Phelps <williamphelps13@gmail.com>
2023-12-10 12:31:08 -0700

    Run tw prettier on Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -18,9 +18,9 @@ export default function Button({
       className={twMerge(
         'rounded-lg py-2 transition-all',
         purpose === 'primary' &&
-          'bg-blue-500  shadow-md hover:bg-blue-500/90 active:bg-blue-600 hover:shadow-lg text-white  focus-visible:outline-none focus:ring-2 focus:ring-blue-800',
+          'bg-blue-500  text-white shadow-md hover:bg-blue-500/90 hover:shadow-lg focus:ring-2  focus:ring-blue-800 focus-visible:outline-none active:bg-blue-600',
         purpose === 'secondary' &&
-          'border-2 text-black border-black  hover:bg-gray-50',
+          'border-2 border-black text-black  hover:bg-gray-50',
         size === 'sm' && 'px-2  text-xs',
         size === 'md' && 'px-4 text-sm',
         size === 'lg' && 'px-6',

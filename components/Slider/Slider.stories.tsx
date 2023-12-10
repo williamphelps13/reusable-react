d6924da5406bea6f4b983e77dc31ad4c8c83774c
William Phelps <williamphelps13@gmail.com>
2023-12-10 12:00:17 -0700

    Create Slider story

---
+++ components/Slider/Slider.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Slider, { type Marks } from './Slider'
+
+const marks: Marks = {
+  0: 'Low',
+  25: 'Med -',
+  50: 'Med',
+  75: 'Med +',
+  100: 'High',
+}
+
+const meta = {
+  component: Slider,
+} satisfies Meta<typeof Slider>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const AllProps: Story = {
+  args: {
+    cId: 'slider',
+    defaultValues: [25, 75],
+    isPearling: true,
+    marks,
+    step: 25,
+  },
+  render: args => <Slider {...args} />,
+}

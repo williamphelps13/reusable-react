959fff5524c10c84726950a253b3acb2497050f8
William Phelps <williamphelps13@gmail.com>
2023-12-16 11:57:45 -0700

    Add badge, loader, and slider exports

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,5 @@
+export { default as Badge } from './Badge/Badge'
 export { default as Button } from './Button/Button'
+export { default as Loader } from './Loader/Loader'
 export { default as Radio } from './Radio/Radio'
+export { default as Slider } from './Slider/Slider'

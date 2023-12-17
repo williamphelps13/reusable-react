8cac5d4e5158b0501405385ebe56fe05b633f6c4
William Phelps <williamphelps13@gmail.com>
2023-12-17 15:56:51 -0700

    Add open in new window icon

--- components/Icon/iconMap.tsx
+++ components/Icon/iconMap.tsx
@@ -1,6 +1,7 @@
 import * as HeroIcons from '@heroicons/react/24/solid'
 
 export const iconMap = {
+  'arrow-top-right-on-square': HeroIcons.ArrowTopRightOnSquareIcon,
   beaker: HeroIcons.BeakerIcon,
   plus: HeroIcons.PlusIcon,
   'x-mark': HeroIcons.XMarkIcon,
